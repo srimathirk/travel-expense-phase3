d0278e22ac5d484bf6ae2c87d7cc71b83f2bfbca
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-08-22 12:21:40 -0400

    seeding trip data

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -20,10 +20,11 @@ if __name__ == '__main__':
     session.query(Expense).delete()
     print("starting seed populates")
     session.commit()
-#insert seed data
+#insert user data
     names = ['Ganes', 'Mauli', 'Heshu', 'Indu', 'krish', 'Sundar', 'Rahman', 'Raja', 'GV', 'Ramya', 'Simma','Sachin']
     emails = ['gane@gmail.com','maUli@yahoo.com', 'Heshu@Yahoo.com', 'Gundu@Hotmail.com', 'Krish@Gmail.com', 'Sundae@gmail.com', 'rahman@raway.com','raja@ymail.com','gv@tamilan.com','ramy@ram.com','simma@gaho.com','sachin@gmail.com']
     phones = ['2318587894', '7414741474', '5588559898','8787878788', '9969658749', '8548785469','7878555544','8585777777', '7523232314', '4455787878','2255888888','7878999999']
+    #seed data
     users = []
     for name,phone,email in zip(names,emails,phones):
         #zip iterates through names,phones,emails lists simultaneously
@@ -37,7 +38,43 @@ if __name__ == '__main__':
         session.add(user)
         users.append(user)
     session.commit()
-    
+
+    #insert tripdata
+    trip_datas = [
+        ('Princeton, New Jersey','Philadelphia',3.82,50,5),
+        ('Catskills, New York','Lexington, Massachusetts',4.75,45,4),
+        ('Finger Lakes, New York', 'Alexandria, Virginia',4.58,60,2),
+        ('Westerly, Rhode Island','Cape May, New Jersey',3.64,50,1),
+        ('Devils Tower, Wyoming','Niagara Falls',4.11,45,3),
+        ('Philadelphia','Southampton Beach, Long Island',4.11,55,5),
+        ('Montreal, Canada', 'New Jersey',3.64,55,4),
+        ('Chincoteague Island, Virginia', 'Franconia, New Hampshire',3.72,55,6),
+        ('The Skyline Drive, Virginia','Jersey City, NJ',3.64,55,7),
+        ('Moab, Utah', 'Camden, Maine',3.77,55,8),
+        ('Austin, Texas','Zion National Park',3.60,55,9),
+        ('Seneca Falls, New York','Princeton, New Jersey',4.05,65,12),
+        ('Southampton Beach, Long Island','Kent, Connecticut',5.5,65,11),
+        ('Killington, Vermont','Big Sur, California',3.81,65,10),
+        ('Hershey, Pennsylvania','Washington DC',3.99,65,8),
+        ('Atlantic City, New Jersey','Hudson Valley, New York',4.54,65,9),
+        ('Brandywine Valley, Pennsylvania', 'Atlantic City, New Jersey',3.25,45,6),
+        ('Kent, Connecticut','The Poconos Mountains',3.17,50,4)
+    ]
+    #seed data
+    trips=[]
+    for start_place,end_place,avg_gas_price,fuel_efficiency_mpg,user_id in trip_datas:
+        trip = Trip(
+            start_place=start_place,
+            end_place=end_place,
+            avg_gas_price=avg_gas_price,
+            fuel_efficiency_mpg=fuel_efficiency_mpg,
+            user_id=user_id
+        )
+        session.add(trip)
+        trips.append(trip)
+    #commiting change
+    session.commit()
+
 
     #import ipdb; ipdb.set_trace()
     session.close()
