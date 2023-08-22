e2dab38cf72b7c9c05c232a2ef7b38fa9e67a231
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-08-22 10:16:39 -0400

    adding User model

--- lib/db/models.py
+++ lib/db/models.py
@@ -10,3 +10,20 @@ metadata = MetaData(naming_convention=convention)
 Base=declarative_base(metadata=metadata)
 #database connection 
 engine = create_engine('sqlite:///travel_records.db')
+
+#User class (table)
+class User(Base):
+    __tablename__='users'
+    user_id = Column(Integer(),primary_key=True)
+    user_name = Column(String())
+    mail_id = Column(String(),unique=True)
+    phone_no = Column(String())
+    created_at = Column(DateTime(),server_default=func.now())
+    
+    trips = relationship("Trip", back_populates='user')
+    def __repr__(self):
+        return f"User {self.user_id}: " \
+            + f"Name {self.user_name}, " \
+            + f"Email {self.mail_id}" \
+            + f"Phone {self.phone_no}" \
+            + f"at {self.created_at}"
