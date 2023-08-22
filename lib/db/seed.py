8c4ab3a8e1ceaf29c58a7e1e0c7da08c54f1795c
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-08-22 10:40:44 -0400

    adding users in seed file

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -20,3 +20,20 @@ if __name__ == '__main__':
     session.query(Expense).delete()
     print("starting seed populates")
     session.commit()
+insert seed data
+    names = ['Ganes', 'Mauli', 'Heshu', 'Indu', 'krish', 'Sundar', 'Rahman', 'Raja', 'GV', 'Ramya', 'Simma','Sachin']
+    emails = ['gane@gmail.com','maUli@yahoo.com', 'Heshu@Yahoo.com', 'Gundu@Hotmail.com', 'Krish@Gmail.com', 'Sundae@gmail.com', 'rahman@raway.com','raja@ymail.com','gv@tamilan.com','ramy@ram.com','simma@gaho.com','sachin@gmail.com']
+    phones = ['2318587894', '7414741474', '5588559898','8787878788', '9969658749', '8548785469','7878555544','8585777777', '7523232314', '4455787878','2255888888','7878999999']
+    users = []
+    for name,phone,email in zip(names,emails,phones):
+        #zip iterates through names,phones,emails lists simultaneously
+        user =  User(
+            user_name = name,
+            mail_id = email,
+            phone_no = phone,
+            created_at = datetime.now()
+        )
+        # add and commit individually to get IDs back
+        session.add(user)
+        users.append(user)
+    session.commit()
